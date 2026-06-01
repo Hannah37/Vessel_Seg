0cd0e9533ba0c0d0543c9c6fb506de795f1e8b3c
Hannah37 <hannah3767@gmail.com>
2026-06-01 19:19:33 -0400

    gen3.sh running w/o disconnection

--- gen3.sh
+++ gen3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=sma_branch3
+#SBATCH --job-name=sma_branch
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:a100:1
 #SBATCH --cpus-per-task=8
@@ -12,11 +12,10 @@ source /vf/users/drdcad/Hyuna/projects/vessel_seg/seg/bin/activate
 
 cd /vf/users/drdcad/Hyuna/projects/vessel_seg/code/Vessel_Seg
 
-python -u gen_vessels_3.py  \
-  --hu_min 60 \
-  --vesselness_thr 0.0003 \
-  --hu_max 380 \
-  --max_radius_vox 6 \
-  --margin 120 \
-  --max_iter 1000 \
-  --min_size 2
+python -u gen_vessels_3.py \
+  --hu_min 50 \
+  --hu_max 370 \
+  --vesselness_thr 0.001 \
+  --margin 95 \
+  --max_iter 700 \
+  --min_size 25
